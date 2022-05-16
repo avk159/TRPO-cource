e24b549e918a488129c68d731b9a8bbcf8cc15b6
avk159 <caviar1337@ya.ru>
2022-05-16 16:33:10 +0700

    Update coverage.xml file

--- Site1/settings.py
+++ Site1/settings.py
@@ -45,7 +45,8 @@ JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
                  'django_jenkins.tasks.run_pep8',
                  'django_jenkins.tasks.run_pyflakes',
                  'django_jenkins.tasks.run_csslint',
-                 'django_jenkins.tasks.run_sloccount'
+                 'django_jenkins.tasks.run_sloccount',
+               #  'django_jenkins.tasks.with_coverage',
                  )
 
 MIDDLEWARE = [
