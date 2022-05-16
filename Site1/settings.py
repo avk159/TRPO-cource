41e45868ff06684bc483458677ec0da25804ba8b
avk159 <caviar1337@ya.ru>
2022-05-16 19:34:43 +0700

    Migrations folder movement

--- Site1/settings.py
+++ Site1/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS += ('django_jenkins',)
 JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
                  'django_jenkins.tasks.run_pep8',
                  'django_jenkins.tasks.run_pyflakes',
+                # 'django_jenkins.tasks.run_jslint',
                  'django_jenkins.tasks.run_csslint',
                  'django_jenkins.tasks.run_sloccount',
                #  'django_jenkins.tasks.with_coverage',
